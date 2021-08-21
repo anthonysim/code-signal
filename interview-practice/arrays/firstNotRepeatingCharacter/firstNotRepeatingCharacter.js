15635b4ad4157e59ef83c346c69f6a6529b4629d
Anthony Sim <anthonyysim@gmail.com>
2021-08-21 13:59:15 -0700

    firstNonRepeat

---
+++ interview-practice/arrays/firstNotRepeatingCharacter/firstNotRepeatingCharacter.js
@@ -0,0 +1,19 @@
+function firstNotRepeatingCharacter(str) {
+  const hash = {};
+  let res = [];
+
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+
+    if (!(char in hash)) {
+      hash[char] = null;
+      res.push([char, i])
+    }
+    hash[char] += 1;
+
+    if (hash[char] > 1) {
+      res = res.filter(num => num[0] !== char);
+    }
+  }
+  return res.length === 0 ? '_' : res[0][0];
+}
