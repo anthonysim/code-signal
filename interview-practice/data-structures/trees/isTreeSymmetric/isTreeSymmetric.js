f252cdd0053512c4c5ac191ffa45ba2a6867b3eb
Anthony Sim <anthonyysim@gmail.com>
2021-08-24 21:39:23 -0700

    tree symmetric

---
+++ interview-practice/data-structures/trees/isTreeSymmetric/isTreeSymmetric.js
@@ -0,0 +1,12 @@
+function isTreeSymmetric(root) {
+  if (root === null) return true;
+
+  function traverse(node1, node2) {
+    if (!node1 && !node2) return true;
+    if (!node1 || !node2) return false;
+    if (node1.value !== node2.value) return false;
+
+    return traverse(node1.left, node2.right) && traverse(node1.right, node2.left)
+  }
+  return traverse(root.left, root.right);
+}
